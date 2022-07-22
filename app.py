0c839bd12de6799cf2660d94f954d51de673d6cd
DeepakParashivamurthy <59639810+DeepakParashivamurthy@users.noreply.github.com>
2022-07-22 15:07:33 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -12,9 +12,24 @@ conn = ibm_db.connect("DATABASE=bludb;HOSTNAME=2d46b6b4-cbf6-40eb-bbce-6251e6ba0
 
 @app.route('/')
 
-def home():
+def home('/'):
     return render_template('register.html')
 
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+
+@app.route('/request')
+def request():
+    return render_template('request.html')
+
+
+@app.route('/stats')
+def stats():
+    return render_template('stats.html')
 
 @app.route('/login',methods =['GET', 'POST'])
 def login():
@@ -40,7 +55,7 @@ def login():
             msg = 'Logged in successfully !'
             
             msg = 'Logged in successfully !'
-            return render_template('dashboard.html', msg = msg)
+            return render_template('register.html', msg = msg)
         else:
             msg = 'Incorrect username / password !'
     return render_template('login.html', msg = msg)
@@ -52,16 +67,14 @@ def login():
 def register():
     msg = ''
     if request.method == 'POST' :
-        name = request.form['name']
+        name = request.form['username']
         email = request.form['email']
 	phone= request.form['phone']
 	city= request.form['city']
 	infect= request.form['infect']
 	blood= request.form['blood']
         password = request.form['password']
-        
-
-
+     
 
         sql = "SELECT * FROM users WHERE username =?"
         stmt = ibm_db.prepare(conn, sql)
@@ -76,9 +89,9 @@ def register():
         elif not re.match(r'[A-Za-z0-9]+', username):
             msg = 'name must contain only characters and numbers !'
         else:
-            insert_sql = "INSERT INTO  users VALUES (?, ?, ?)"
+            insert_sql = "INSERT INTO  users VALUES (?, ?, ?,?,?,?,?)"
             prep_stmt = ibm_db.prepare(conn, insert_sql)
-            ibm_db.bind_param(prep_stmt, 1, name)
+            ibm_db.bind_param(prep_stmt, 1, username)
             ibm_db.bind_param(prep_stmt, 2, email)
             ibm_db.bind_param(prep_stmt, 3, phone)
 	    ibm_db.bind_param(prep_stmt, 4, city)
@@ -92,10 +105,83 @@ def register():
         msg = 'Please fill out the form !'
     return render_template('register.html', msg = msg)
 
+
+@app.route('/request',methods =['GET','POST'])
+def request():
+    global userid
+    msg = ''
+   
+  
+    if request.method == 'POST' :
+        username = request.form['bloodgroup']
+        password = request.form['address']
+        sql = "SELECT * FROM users WHERE username =? AND password=?"
+        stmt = ibm_db.prepare(conn, sql)
+        ibm_db.bind_param(stmt,1,bloodgroup)
+        ibm_db.bind_param(stmt,2,address)
+        ibm_db.execute(stmt)
+        account = ibm_db.fetch_assoc(stmt)
+        print (account)
+        if account:
+            session['loggedin'] = True
+            session['id'] = account['USERNAME']
+            userid=  account['USERNAME']
+            session['username'] = account['USERNAME']
+            msg = 'Logged in successfully !'
+            
+            msg = 'Logged in successfully !'
+            return render_template('request.html', msg = msg)
+	    else:
+            insert_sql = "INSERT INTO  requester VALUES (?, ?)"
+            prep_stmt = ibm_db.prepare(conn, insert_sql)
+            ibm_db.bind_param(prep_stmt, 1, bloodgroup)
+            ibm_db.bind_param(prep_stmt, 2, address)
+           
+            ibm_db.execute(prep_stmt)
+            msg = ' Requested Successfully!'
+        else:
+            msg = 'Incorrect username / password !'
+    return render_template('login.html', msg = msg)
+
+
+
+
+
+
+@app.route('/stats',methods =['POST'])
+def stats():
+    global userid
+    msg = ''
+   
+  
+    if request.method == 'POST' :
+        username = request.form['username']
+        password = request.form['password']
+        sql = "SELECT * FROM users WHERE username =? AND password=?"
+        stmt = ibm_db.prepare(conn, sql)
+        ibm_db.bind_param(stmt,1,username)
+        ibm_db.bind_param(stmt,2,password)
+        ibm_db.execute(stmt)
+        account = ibm_db.fetch_assoc(stmt)
+        print (account)
+        if account:
+            session['loggedin'] = True
+            session['id'] = account['USERNAME']
+            userid=  account['USERNAME']
+            session['username'] = account['USERNAME']
+            msg = 'Logged in successfully !'
+            
+            msg = 'Logged in successfully !'
+            return render_template('stats.html', msg = msg)
+        else:
+            msg = 'Incorrect username / password !'
+    return render_template('login.html', msg = msg)
+
+
 @app.route('/dashboard')
 def dash():
     
-    return render_template('dashboard.html')      
+    return render_template('stats.html')      
          
     
 
@@ -106,7 +192,7 @@ def display():
     cursor = mysql.connection.cursor()
     cursor.execute('SELECT * FROM job WHERE userid = % s', (session['id'],))
     account = cursor.fetchone()
-    print("accountdislay",account)
+    print("accountdisplay",account)
 
     
     return render_template('display.html',account = account)
